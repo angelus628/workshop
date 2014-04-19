c4cf66bfb12e32cf75861cb1ed07f623fbdb8da0
Luis Ángel <angelus628@hotmail.com>
2014-04-19 15:53:13 -0500

    Se agregó la funcionalidad de transiones para cada página

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -4,40 +4,44 @@ var EmployeeView = function(adapter, template, employee){
 		this.el.on('click', '.change-pic-btn', this.changePicture);
 
 		//Add add location
-		this.el.on('click', '.add-location-btn', function(event){
-			event.preventDefault();
-		    navigator.geolocation.getCurrentPosition(
-		        function(position) {
-		            alert(position.coords.latitude + ',' + position.coords.longitude);
-		        },
-		        function() {
-		            alert('Error al obtenetener la ubicación actual');
-		        });
-		    return false;
-		});
+		this.el.on('click', '.add-location-btn', this.addLocation);
 
 		//Add contacs
-		this.el.on('click', '.add-contact-btn', function(event){
-			event.preventDefault();
-		    console.log('addToContacts');
-		    if (!navigator.contacts) {
-		        alert("No existe soporte para el API de contactos", "Error");
-		        return;
-		    }
-		    var contact = navigator.contacts.create();
-		    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
-		    var phoneNumbers = [];
-		    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
-		    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
-		    contact.phoneNumbers = phoneNumbers;
-		    contact.save();
-		    return false;
-		});
+		this.el.on('click', '.add-contact-btn', this.addToContacts);
 	};
 
 	this.render = function(){
 		this.el.html(template(employee));
 		return this;
+	};
+
+	this.addToContacts = function(){
+		event.preventDefault();
+	    console.log('addToContacts');
+	    if (!navigator.contacts) {
+	        alert("No existe soporte para el API de contactos", "Error");
+	        return;
+	    }
+	    var contact = navigator.contacts.create();
+	    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+	    var phoneNumbers = [];
+	    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+	    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+	    contact.phoneNumbers = phoneNumbers;
+	    contact.save();
+	    return false;
+	}
+
+	this.addLocation = function(){
+		event.preventDefault();
+	    navigator.geolocation.getCurrentPosition(
+	        function(position) {
+	            alert(position.coords.latitude + ',' + position.coords.longitude);
+	        },
+	        function() {
+	            alert('Error al obtenetener la ubicación actual');
+	        });
+	    return false;
 	};
 
 	this.changePicture = function(){
